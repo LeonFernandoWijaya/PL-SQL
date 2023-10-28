7e1ced01af5c55b7825111adb3d09f5edb9b9b81
Leon <120233447+LeonFernandoWijaya@users.noreply.github.com>
2023-10-28 14:49:08 +0700

    Update PLSQl 1.sql

--- PLSQl 1.sql
+++ PLSQl 1.sql
@@ -1,8 +1,8 @@
-1. Buatlah  program  PL/SQL  menghitung  gaji  bersih  karyawan  yang  memiliki  id  =  101 dengan ketentuan: 
- Jika gaji <5000 maka nilai gaji bersih adalah gaji – pajak sebesar 1% dari gaji.
- Jika gaji <10000 maka nilai gaji bersih adalah gaji –pajak sebesar 2% dari gaji.
- Jika selaiannya, nilai gaji bersih adalah gaji –pajak sebesar 3% dari gaji.
-Gunakan tabel employees kolom salary.
+--1. Buatlah  program  PL/SQL  menghitung  gaji  bersih  karyawan  yang  memiliki  id  =  101 dengan ketentuan: 
+-- Jika gaji <5000 maka nilai gaji bersih adalah gaji – pajak sebesar 1% dari gaji.
+-- Jika gaji <10000 maka nilai gaji bersih adalah gaji –pajak sebesar 2% dari gaji.
+-- Jika selaiannya, nilai gaji bersih adalah gaji –pajak sebesar 3% dari gaji.
+--Gunakan tabel employees kolom salary.
 
 DECLARE
 v_emp_id employees.employee_id%TYPE;
@@ -20,10 +20,10 @@ DBMS_OUTPUT.PUT_LINE('Id Karyawan = ' || v_emp_id || ' gaji kotor = ' || v_salar
 END IF;
 END;
 
-2. Buatlah  kode  program  PL/SQL  untuk  melakukan  statemen  select  employee  yang memiliki manager id = 300. Program harus dilengkapi dengan exception handling untuk menangani kondisi error:
--Jika  statemen  select  mengembalikan  hasil  lebih  dari  satu  baris  tampilkan  pesan ‘Data  yang Anda cari tidak ditemukan.’
--Jika statemen select tidak mengembalikan hasil tampilkan pesan ‘Terdapat data lebih  dari  1 baris.’
--Selain kedua kondisi di atas tampilkan pesan ‘ Terjadi error lainnya.’ 
+--Buatlah  kode  program  PL/SQL  untuk  melakukan  statemen  select  employee  yang memiliki manager id = 300. Program harus dilengkapi dengan exception handling untuk menangani kondisi error:
+--Jika  statemen  select  mengembalikan  hasil  lebih  dari  satu  baris  tampilkan  pesan ‘Data  yang Anda cari tidak ditemukan.’
+--Jika statemen select tidak mengembalikan hasil tampilkan pesan ‘Terdapat data lebih  dari  1 baris.’
+--Selain kedua kondisi di atas tampilkan pesan ‘ Terjadi error lainnya.’ 
 
 DECLARE
 v_lname VARCHAR2(15);
@@ -40,9 +40,9 @@ WHEN OTHERS THEN
 DBMS_OUTPUT.PUT_LINE('terjadi error lainnya.');
 END;
 
-3.  Buatlah    code    program    PL/SQL    menggunakan cursor    with    parametersuntuk menampilkan capitol, country name, dan currency! 
-Tampilkan maksimal 5 baris output dan urutkan capitol berdasarkan alfabet. 
-Gunakan tabel countries dan currency.
+--3.  Buatlah    code    program    PL/SQL    menggunakan cursor    with    parametersuntuk menampilkan capitol, country name, dan currency! 
+--Tampilkan maksimal 5 baris output dan urutkan capitol berdasarkan alfabet. 
+--Gunakan tabel countries dan currency.
 
 DECLARE 
 v_currency_name currencies.currency_name%TYPE;
@@ -63,8 +63,8 @@ END LOOP;
 CLOSE cur_country;
 END;
 
-4.  Buatlah  code  program  PL/SQL  untuk  menampilkan  nama  profesi  (Job  title)  dan  nama karyawan yang memiliki profesi tersebut! 
-Gunakan tabel employees dan tabel jobs. 
+--4.  Buatlah  code  program  PL/SQL  untuk  menampilkan  nama  profesi  (Job  title)  dan  nama karyawan yang memiliki profesi tersebut! 
+--Gunakan tabel employees dan tabel jobs. 
 
 DECLARE
 CURSOR cur_job IS SELECT * FROM jobs
